2dac0df56d14d5f370c7ea8d5b1a0a1f661a036c
kartikayakulshrestha <kartikaya.kul0908@gmail.com>
2024-08-10 20:47:59 +0530

    small update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   
   const queryParams = new URLSearchParams(location.search);
   // let to const token
-  let token = queryParams.get("token");
+  const token = queryParams.get("token");
   const localStorageToken = localStorage.getItem("token")
   //productionchange
   //token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoia2FydGlrYXlhLmt1bDA5MDhAZ21haWwuY29tIiwiaWQiOjUwNiwiZmlyc3ROYW1lIjoiS2FydGlrYXlhIiwibGFzdE5hbWUiOiJLdWxzaHJlc3RoYSJ9LCJpYXQiOjE3MjMyODU3ODMsImV4cCI6MTc1NDgyMTc4M30.o_BmQHlIKF0_74-IeR58AM4aPAxmLZBKRU7WVA7SnL0"
